42917bc3e9ea4247dfa8fbff92cf3d5052d48c8f
andrew <ceddoy@mail.ru>
2022-02-21 15:58:33 +0700

    добавил по дефолту аутентификацию по токену

--- aggregator_available_logs/settings.py
+++ aggregator_available_logs/settings.py
@@ -94,3 +94,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
