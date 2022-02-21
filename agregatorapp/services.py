72dd2e82c8badca85129cecd0a69727686d9f579
andrew <ceddoy@mail.ru>
2022-02-22 02:06:36 +0700

    поправил путь файла и его название.

--- agregatorapp/services.py
+++ agregatorapp/services.py
@@ -1,12 +1,17 @@
+
 import apache_log_parser
 
 from aggregator_available_logs.settings import PATH_ACCESS_LOG, MASK_REQUEST_LOGS
 from agregatorapp.models import Log
 
 
+# def get_date_for_retrieve_logs():
+#     return (datetime.date.today() - datetime.timedelta(days=1)).strftime("%d-%m-%Y")
+
+
 def load_logs():
     data_in_bd = []
-    with open(PATH_ACCESS_LOG, 'r') as f:
+    with open(f'{PATH_ACCESS_LOG}access.log', 'r') as f:
         line_parser = apache_log_parser.make_parser(MASK_REQUEST_LOGS)
         for line_log in f.read().splitlines():
             if not line_log:
