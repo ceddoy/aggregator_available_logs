911038bfa498b4369f834d4201d79ad02fcf2a7e
andrew <ceddoy@mail.ru>
2022-02-22 06:19:26 +0700

    добавил оптимизацию запросов в БД для филтров

--- agregatorapp/filters.py
+++ agregatorapp/filters.py
@@ -1,9 +1,10 @@
 import datetime
 
-from django_filters.rest_framework import FilterSet, filters, RangeFilter, DateFromToRangeFilter
+from django_filters.rest_framework import FilterSet, filters, RangeFilter
 from django_filters.fields import DateRangeField
 
 from agregatorapp.models import Log
+from agregatorapp.services import get_date_end_day
 
 
 class CustomDateRangeField(DateRangeField):
@@ -14,9 +15,9 @@ class CustomDateRangeField(DateRangeField):
                 stop_date = datetime.datetime.max
             elif not start_date and stop_date:
                 start_date = datetime.datetime.min
-                stop_date += datetime.timedelta(days=1)
+                stop_date = get_date_end_day(stop_date)
             else:
-                stop_date += datetime.timedelta(days=1)
+                stop_date = get_date_end_day(stop_date)
             return start_date, stop_date
         return None
 
@@ -26,14 +27,19 @@ class CustomDateFromToRangeFilter(RangeFilter):
 
 
 class LogsFilter(FilterSet):
-
-    ip_address = filters.CharFilter(lookup_expr='exact')
-    date_add = filters.DateFilter(lookup_expr='contains')
+    ip_address = filters.CharFilter(lookup_expr='exact', method='ip_address_filter')
+    date_add = filters.DateFilter(lookup_expr='contains', method='date_add_filter')
     date_range = CustomDateFromToRangeFilter(lookup_expr='contains', method='date_range_filter')
 
     class Meta:
         model = Log
         fields = ['ip_address', 'date_add', 'date_range']
 
+    def ip_address_filter(self, queryset, name, value):
+        return queryset.filter(ip_address=value).order_by('date_add')
+
     def date_range_filter(self, queryset, name, value):
         return queryset.filter(date_add__range=value)
+
+    def date_add_filter(self, queryset, name, value):
+        return queryset.filter(date_add__range=(value, get_date_end_day(value)))
