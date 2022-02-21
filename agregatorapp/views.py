6c98f6ba00d69809e4eed730321465332847c862
andrew <ceddoy@mail.ru>
2022-02-21 17:21:06 +0700

    добавид во LogListAPIView классы: фильтер и права доступа

--- agregatorapp/views.py
+++ agregatorapp/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
+from agregatorapp.filters import LogsFilter
 from agregatorapp.models import Log
 from agregatorapp.serializer import LogListModelSerializer
 
@@ -7,3 +9,5 @@ from agregatorapp.serializer import LogListModelSerializer
 class LogListAPIView(ListAPIView):
     serializer_class = LogListModelSerializer
     queryset = Log.objects.all()
+    filterset_class = LogsFilter
+    permission_classes = [IsAuthenticated, ]
